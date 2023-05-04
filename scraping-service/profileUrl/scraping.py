9ef8470964d9b3c25611ad2679015a015ac52b7c
meliwi <melissa.gonzalez@correounivalle.edu.co>
2023-05-03 22:49:56 -0500

    uncommented frontend microservice

--- scraping-service/profileUrl/scraping.py
+++ scraping-service/profileUrl/scraping.py
@@ -82,7 +82,7 @@ def get_gitlab_information(profile_url):
     def get_links(projects_type):
         #Set remote web driver. Recives the url of the remote web server (selenium container) and options. 
         driver = webdriver.Remote(
-                command_executor='http://172.18.0.2:4444',
+                command_executor='http://172.19.0.3:4444',
                 options=chrome_options
             )
         # Opening gitlab's user profile
@@ -140,7 +140,7 @@ def get_gitlab_information(profile_url):
     #Obtains the technologies used in the project repository from the link provided
     def scrape_page(link):
             driver = webdriver.Remote(
-                command_executor='http://172.18.0.2:4444',
+                command_executor='http://172.19.0.3:4444',
                 options=chrome_options
             )
             driver.get(link)
