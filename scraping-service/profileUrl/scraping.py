766e0a23d19fc9642cffa8e662138dee2ebf35f0
meliwi <melissa.gonzalez@correounivalle.edu.co>
2023-05-31 23:23:17 -0500

    scraping error fixed

--- scraping-service/profileUrl/scraping.py
+++ scraping-service/profileUrl/scraping.py
@@ -141,11 +141,7 @@ def get_gitlab_information(profile_url):
     #Obtains the technologies used in the project repository from the link provided
     def scrape_page(link):
             driver = webdriver.Remote(
-<<<<<<< HEAD
                 command_executor='http://172.19.0.6:4444',
-=======
-                command_executor='http://172.18.0.4:4444',
->>>>>>> TM-26-main
                 options=chrome_options
             )
             driver.get(link)
